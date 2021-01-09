394bd811ecafafc872a44e1d5b23408355b98263
Cristobal Szkutnik <crisszkutnik@gmail.com>
2021-01-08 22:18:19 -0300

    Changed order of execution of complete_setup.sh

--- complete_setup.sh
+++ complete_setup.sh
@@ -5,14 +5,14 @@ sudo apt-get install -y xclip
 sudo apt-get install curl
 ./alias.sh # Set up alias
 
-# Install kde-plasma
-sudo apt install -y tasksel
-sudo apt install -y kubuntu-desktop
-
-
 # Execute other scripts
 # Remember to give the corresponding permissions to each script (chmod)
 
 ./dev_tools.sh
 ./programs.sh
 ./alias.sh
+
+# Install kde-plasma
+# This takes a lot of time so it goes last
+sudo apt install -y tasksel
+sudo apt install -y kubuntu-desktop
