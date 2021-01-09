d024ecf5a16c3ba9973cd6b46d0dc4a538cdec8e
Cristobal Szkutnik <crisszkutnik@gmail.com>
2021-01-09 12:08:34 -0300

    Added missing apt update

--- dev_tools.sh
+++ dev_tools.sh
@@ -4,6 +4,7 @@ cd ~
 # by default it will install node 14 (LTS)
 curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
+sudo apt update
 sudo apt install -y nodejs
 
 # python
