0a48efaaadcb0d42a47786fa7e8ff1489a7c7938
Cristobal Szkutnik <crisszkutnik@gmail.com>
2022-01-10 12:24:00 -0300

    Update dev_tools.sh

--- scripts/dev_tools.sh
+++ scripts/dev_tools.sh
@@ -1,11 +1,11 @@
 cd ~
 
-# must install curl first
-# by default it will install node 14 (LTS)
-curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
 sudo apt update
-sudo apt install -y nodejs
+
+# must install curl first
+# by default it will install node (LTS)
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt-get install -y nodejs
 rm nodesource_setup.sh
 
 sudo npm install -g typescript
@@ -15,3 +15,14 @@ sudo npm install -g sass
 sudo apt-get install -y python3
 sudo apt-get install -y python3-pip
 
+# gh cli
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install -y gh
+
+sudo apt install bat
+
+# zsh and themes
+sudo apt install -y zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
